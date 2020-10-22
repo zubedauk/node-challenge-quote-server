a9fe11f6bd1bd7a1023d1cb7fe7bc358e622d880
zubeda <zubedauk@gmail.com>
2020-10-22 03:28:06 +0100

    level2

---
+++ quotes-react-app/src/Quotes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const Quotes=()=>{
+    return(
+        <h1>csc</h1>
+    )
+}
+export default Quotes
--- server.js
+++ server.js
@@ -4,6 +4,15 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
+const lodash=require("lodash")
+const cors=require("cors")
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+app.use(logger('dev'));
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 //load the quotes JSON
 const quotes = require("./quotes.json");
@@ -18,13 +27,49 @@ app.get("/", function (request, response) {
 
 //START OF YOUR CODE...
 app.get("/quotes",function(req,response){
+ 
   response.send(quotes);
+  
 })
+app.get("/quotes/echo",function(req,response){
+  const query=req.query.word;
+  response.send(`you said ${query}`)
+  
+  
+  
+})
+app.get("/quotes/search",function(req,response){
+  const query=req.query.term;
+  response.send(search(quotes,query))
+  
+})
+function search(quotes,term){
+  let t=quotes.filter(function(obj){
+    return obj.quote.toLowerCase().includes(term.toLowerCase())
+    ||
+    obj.author.toLowerCase().includes(term.toLowerCase());
+  })
+  return t;
+}
 app.get("/quotes/random",function(request,response){
-  response.send(pickFromArray(quotes))
+  //response.send(pickFromArray(quotes))
+  response.send(lodash.sample(quotes))
 })
-app.get(`${process.env.PORT}/quotes/random/test`,function(request,response){
-  response.send("test")
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+app.get('/products/:id', cors(), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for a Single Route'})
+})
+
+app.get("/test",function(request,response){
+  response.sendFile(__dirname+"/index.html")
+})
+app.post("/test",function(request,response){
+  
+  console.log(request.body)
+  const name=request.body.v1
+  response.send(name)
 })
 
 //...END OF YOUR CODE
@@ -38,6 +83,7 @@ function pickFromArray(arr) {
 }
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
+//process.env.PORT
+const listener = app.listen(process.env.PORT||3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
